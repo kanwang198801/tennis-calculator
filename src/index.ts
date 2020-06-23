6c132f3039d54cd4a12d09c0c19bbbc511a06682
kan <kan.wang@arq.group>
2020-06-23 23:20:57 +1000

    init project

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { MatchType, PlayerType } from './types';
+
+const matches: MatchType[] = [];
+const players: PlayerType[] = [];
---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export interface MatchType {
+   matchNumber: number;
+   player1Name: string;
+   player2Name: string;
+   winnerName: string;
+   player1Sets: number;
+   player2Sets: number;
+}
+
+export interface PlayerType {
+   playerName: string;
+   winCount: number;
+   loseCount: number;
+}
